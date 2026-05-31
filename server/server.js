27fb363d9cf20dd1c5727911a72f8e6f90a29e82
quincy-o <quincyeodom@gmail.com>
2026-05-30 23:55:03 -0700

    Removed redundant listen

--- server/server.js
+++ server/server.js
@@ -13,8 +13,6 @@ const { data } = require("react-router-dom");
 
 const app  = express();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-
 
 // Middleware — mount BEFORE any route.
 
