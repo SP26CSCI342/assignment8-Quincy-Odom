84aa026510b2a9be812de20c43114a3dbd95f5e1
quincy-o <quincyeodom@gmail.com>
2026-05-31 00:17:40 -0700

    fixed a slight issue with fetch

--- src/forms/SignupForm.jsx
+++ src/forms/SignupForm.jsx
@@ -40,7 +40,7 @@ function SignupForm() {
 
     const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
     try {
-      const response = await fetch(`{baseURL}/api/register`, {
+      const response = await fetch(`${baseURL}/api/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password }),
